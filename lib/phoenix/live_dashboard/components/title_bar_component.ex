a4487a9da3a7020b52adceedd85a615613ec95ea
Solomon White <solomon@blinker.com>
2020-10-14 10:47:57 -0600

    updates to work with phoenix_live_dashboard 0.15-dev

--- lib/phoenix/live_dashboard/components/title_bar_component.ex
+++ lib/phoenix/live_dashboard/components/title_bar_component.ex
@@ -10,7 +10,7 @@ defmodule Phoenix.LiveDashboard.TitleBarComponent do
     <div class="<%= @class %>">
       <section>
         <div class="d-flex justify-content-between">
-          <%= @inner_content.([]) %>
+          <%= render_inner(@inner_content) %>
         </div>
         <div class="progress flex-grow-1 mt-2">
           <div
